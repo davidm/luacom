e2c9ddde90e785bbf82b1b2fcae45cb75e9e772d
David Manura <dm.git@math2.org>
2011-02-19 21:56:55 -0500

    Remove helper Lua script for pre-5.1 Lua.

--- src/dll/luacom.lua
+++
@@ -1,8 +0,0 @@
--- loads luacom.dll for pre-5.1 versions of Lua.
-if not luacom then
-  local init, err1, err2 = loadlib("luacom.dll","luacom_openlib")
-  assert (init, (err1 or '')..(err2 or ''))
-  init()
-end
-
-return nil
